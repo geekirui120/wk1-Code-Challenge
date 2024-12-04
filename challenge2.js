32a341f139232f4e9e66216270551c77ed582641
Geekirui120 <geekirui120@gmail.com>
2024-12-04 19:21:22 +0300

    challenge2.js complete

--- challenge2.js
+++ challenge2.js
@@ -1,19 +1,18 @@
-function speedDetector() {
-    const speed = prompt("Enter the speed of the car:");
+function speedDetector(speed) {
     //lets set the speed limit to 70 for every 5km
     const limit = 70;
     const kmPerPoint = 5;
 
     if (speed <= limit) {
-        console.log("Ok");
+        return 'ok';
     } else {
         const points = Math.floor((speed - limit) / kmPerPoint);
         if (points > 12) {
-            console.log("License suspended");
+            return "License suspended";
         } else {
-            console.log(`Points: ${points}`);
+            return `Points: ${points}`;
         }
+        
     }
-}
-
-speedDetector();
+}  
+    console.log(speedDetector(85));
